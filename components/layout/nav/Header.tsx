1b0b787c8d0a98085c7e0d068d4c2535d7eeebcb
mikk.villem <mikk.villem@gmail.com>
2023-12-10 16:32:04 +0200

    add nav styles

--- components/layout/nav/Header.tsx
+++ components/layout/nav/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import NavSheet from './NavSheet';
 import Image from 'next/image';
 import PilatesLogoTransparent from '../../../public/pilates_logo_transparent.png';
+import Link from 'next/link';
 
 type Props = {};
 
@@ -14,18 +15,35 @@ const navMap = {
 
 const Header = (props: Props) => {
   return (
-    <header className="w-full flex justify-between sm:justify-center items-center relative py-2">
-      <div className="w-48">
-        <Image
-          src={PilatesLogoTransparent}
-          alt="Erika Viira Pilatese Akadeemia logo"
-          className=""
-        />
+    <header className="py-2 space-y-4">
+      <div className="w-full flex justify-between sm:justify-center items-center relative ">
+        <div className="w-48 md:w-96">
+          <Image
+            src={PilatesLogoTransparent}
+            alt="Erika Viira Pilatese Akadeemia logo"
+            className=""
+          />
+        </div>
+        <div className="md:hidden">
+          <NavSheet
+            className="sm:absolute sm:right-0 sm:transform sm:-translate-y-1/2"
+            navMap={navMap}
+          />
+        </div>
       </div>
-      <NavSheet
-        className="sm:absolute sm:right-0 sm:transform sm:-translate-y-1/2"
-        navMap={navMap}
-      />
+      <div className="hidden md:flex md:flex-row md:justify-center md:items-center gap-28">
+        {Object.entries(navMap).map(([name, link]) => {
+          return (
+            <Link
+              key={name}
+              className="text-lg font-merriweather-bold capitalize"
+              href={link}
+            >
+              {name}
+            </Link>
+          );
+        })}
+      </div>
     </header>
   );
 };
