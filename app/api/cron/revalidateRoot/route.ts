2b430fea68cf3acbd8f474a90bf9930efa1af8c2
mikk.villem <mikk.villem@gmail.com>
2024-01-02 03:00:21 +0200

    return a response from handler

--- app/api/cron/revalidateRoot/route.ts
+++ app/api/cron/revalidateRoot/route.ts
@@ -1,6 +1,7 @@
 import { revalidatePath } from 'next/cache';
 import { NextRequest } from 'next/server';
 
-export async function GET(_request: NextRequest) {
+export async function GET(request: NextRequest) {
   revalidatePath('/', 'page');
+  return new Response('root updated');
 }
