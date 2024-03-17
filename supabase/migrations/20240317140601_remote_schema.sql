2ac0dc1da236606996366d7d710b7bdaa1456fe2
Mikk Villem <mikk.villem@gmail.com>
2024-03-17 16:25:36 +0200

    add supabase for local dev

---
+++ supabase/migrations/20240317140601_remote_schema.sql
@@ -0,0 +1,77 @@
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+CREATE EXTENSION IF NOT EXISTS "pgsodium" WITH SCHEMA "pgsodium";
+
+CREATE SCHEMA IF NOT EXISTS "public";
+
+ALTER SCHEMA "public" OWNER TO "pg_database_owner";
+
+COMMENT ON SCHEMA "public" IS 'standard public schema';
+
+CREATE EXTENSION IF NOT EXISTS "pg_graphql" WITH SCHEMA "graphql";
+
+CREATE EXTENSION IF NOT EXISTS "pg_stat_statements" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "pgjwt" WITH SCHEMA "extensions";
+
+CREATE EXTENSION IF NOT EXISTS "supabase_vault" WITH SCHEMA "vault";
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA "extensions";
+
+SET default_tablespace = '';
+
+SET default_table_access_method = "heap";
+
+CREATE TABLE IF NOT EXISTS "public"."workouts" (
+    "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
+    "day" smallint NOT NULL,
+    "time" "text" NOT NULL,
+    "duration" smallint DEFAULT '60'::smallint NOT NULL,
+    "name" "text" NOT NULL,
+    "instructor" "text",
+    CONSTRAINT "workout_day_check" CHECK ((("day" > 0) AND ("day" < 8))),
+    CONSTRAINT "workout_duration_check" CHECK (("duration" > 0))
+);
+
+ALTER TABLE "public"."workouts" OWNER TO "postgres";
+
+ALTER TABLE ONLY "public"."workouts"
+    ADD CONSTRAINT "workout_pkey" PRIMARY KEY ("day", "time");
+
+GRANT USAGE ON SCHEMA "public" TO "postgres";
+GRANT USAGE ON SCHEMA "public" TO "anon";
+GRANT USAGE ON SCHEMA "public" TO "authenticated";
+GRANT USAGE ON SCHEMA "public" TO "service_role";
+
+GRANT ALL ON TABLE "public"."workouts" TO "anon";
+GRANT ALL ON TABLE "public"."workouts" TO "authenticated";
+GRANT ALL ON TABLE "public"."workouts" TO "service_role";
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "service_role";
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON FUNCTIONS  TO "service_role";
+
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "postgres";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "anon";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "authenticated";
+ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON TABLES  TO "service_role";
+
+RESET ALL;
---
+++ supabase/seed.sql
@@ -0,0 +1,17 @@
+INSERT INTO "public"."workouts" ("day", "time", "duration", "name", "instructor") VALUES
+	(1, '18:00', 60, 'L1', 'Erika'),
+	(2, '10:00', 60, 'Baas / L1', 'Erika'),
+	(2, '16:30', 60, 'L1 / L2', 'Erika'),
+	(2, '17:30', 60, 'L1', 'Erika'),
+	(3, '17:00', 60, 'Start-tund', 'Erika'),
+	(4, '10:00', 60, 'Baas / L1', 'Erika'),
+	(4, '16:30', 60, 'L2', 'Erika'),
+	(4, '17:30', 60, 'L1', 'Erika'),
+	(7, '11:00', 60, 'Start-tund', 'Erika'),
+	(2, '08:15', 60, 'Võimleme koos', 'Erika'),
+	(4, '08:15', 60, 'Võimleme koos', 'Erika'),
+	(3, '12:00', 60, 'Pilates kõigile', 'Erika'),
+	(1, '12:00', 60, 'Pilates kõigile', 'Erika'),
+	(3, '18:00', 60, 'Yin jooga', 'Erika'),
+	(5, '12:00', 60, 'Yin jooga', 'Erika'),
+	(7, '10:00', 60, 'L1', 'Erika');
